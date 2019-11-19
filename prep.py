98531ead899236d05fb9c0c50834cc3cdc14059c
Symeon White <symeonwhite@Symeons-MacBook-Pro.local>
2019-11-19 11:03:56 -0600

    Changed str and int to floats

--- prep.py
+++ prep.py
@@ -5,4 +5,11 @@ def prep_df():
     df = get_df()
     df["Date"] = pd.to_datetime(df.Date)
     df = df.set_index(["Date"])
+    col_int = ["Floors", "Distance", "Minutes Fairly Active", "Minutes Lightly Active", "Minutes Very Active"]
+    for n in col_int:
+        df[n] = df[n].astype(float)
+        print([n])
+    col = ["Calories Burned","Activity Calories","Minutes Sedentary", "Steps"]
+    for n in col:
+        df[n] = df[n].str.replace(",", "").astype(float)
     return df
