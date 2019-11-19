32956309b0159c070966a88c38c5f9bc8d58c249
Symeon White <symeonwhite@Symeons-MacBook-Pro.local>
2019-11-19 10:17:27 -0600

    acquire,prep work

---
+++ acquire.py
@@ -0,0 +1,12 @@
+import pandas as pd
+def get_df():
+    csv1 = pd.read_csv("april-may.csv")
+    csv2 = pd.read_csv("may-june.csv")
+    csv3 = pd.read_csv("june-july.csv")
+    csv4 = pd.read_csv("july-aug.csv")
+    csv5 = pd.read_csv("aug-sep.csv")
+    csv6 = pd.read_csv("sep-oct.csv")
+    csv7 = pd.read_csv("oct-nov.csv")
+    csv8 = pd.read_csv("nov-dec.csv")
+    frames = [csv1, csv2, csv3, csv4, csv5, csv6, csv7, csv8]
+    return pd.concat(frames)
---
+++ prep.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from acquire import get_df
+
+def prep_df():
+    df = get_df()
+    df["Date"] = pd.to_datetime(df.Date)
+    df.set_index(["Date"])
+    return df
