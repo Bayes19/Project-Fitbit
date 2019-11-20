cc1cdc2c82b1ba40fa0457b57d005f86f39e3780
Symeon White <symeonwhite@Symeons-MacBook-Pro.local>
2019-11-19 20:02:31 -0600

    commit

--- explore.py
+++ explore.py
@@ -6,19 +6,6 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 df = prep_df()
 df.isnull().sum()
-X = df.Date
-y = df.Steps
-
-X = np.array(X)
-y = np.array(y)
-
-from sklearn.model_selection import TimeSeriesSplit
-tss = TimeSeriesSplit(n_splits=4, max_train_size=None)
-
-for train_index, test_index in tss.split(X):
-    print("TRAIN:", train_index, "TEST:", test_index)
-    X_train, X_test = X[train_index], X[test_index]
-    y_train, y_test = y[train_index], y[test_index]
 
 
 #all graphs
