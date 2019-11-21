af21f87de33189c2a5f6a187c972176818a37785
Matt Santos <mattsantos541@gmail.com>
2019-11-20 23:20:20 -0600

    prohpet prep

--- model.py
+++ model.py
@@ -1,36 +1,9 @@
 import pandas as pd
 from prep import prep_df, time_split_2, test_train_split
 import seaborn as sns
-df = prep_df()
-X_train, y_train, X_test, y_test = time_split_2(df)
-train, test = test_train_split(df, .66)
 
-def evaluate(target_var, train = train, test = test, output=True):
-    mse = metrics.mean_squared_error(test[target_var], yhat[target_var])
-    rmse = math.sqrt(mse)
 
-    if output:
-        print('MSE:  {}'.format(mse))
-        print('RMSE: {}'.format(rmse))
-    else:
-        return mse, rmse
 
-def plot_and_eval(target_vars, train = train, test = test, metric_fmt = '{:.2f}', linewidth = 4):
-    if type(target_vars) is not list:
-        target_vars = [target_vars]
-
-    plt.figure(figsize=(16, 8))
-    plt.plot(train[target_vars],label='Train', linewidth=1)
-    plt.plot(test[target_vars], label='Test', linewidth=1)
-
-    for var in target_vars:
-        mse, rmse = evaluate(target_var = var, train = train, test = test, output=False)
-        plt.plot(yhat[var], linewidth=linewidth)
-        print(f'{var} -- MSE: {metric_fmt} RMSE: {metric_fmt}'.format(mse, rmse))
-
-    plt.show()
-
-    yhat[var] = pd.DataFrame(model.forecast(test[var].shape[0]), columns=[var])
 
 def next_two_weeks_Holt(df):
     import matplotlib.pyplot as plt
@@ -43,7 +16,7 @@ def next_two_weeks_Holt(df):
     return final
 
 
-<<<<<<< HEAD
+'''<<<<<<< HEAD
 =======
 def lineplot(df):
     from sklearn.preprocessing import MinMaxScaler
@@ -55,7 +28,7 @@ def lineplot(df):
         ,6:"Minutes Fairly Active",7:"Minutes Very Active", 8:"Activity Calories", 9:"BMR"})
     df2 = df2.set_index(df.index)
     return df2
->>>>>>> 8b998b1bfea4b5611b6d35a8d1d3f3d6ce1f2103
+>>>>>>> 8b998b1bfea4b5611b6d35a8d1d3f3d6ce1f2103'''
 
 def all_decompose(df):
     df = df.set_index(["Date"])
@@ -65,4 +38,22 @@ def all_decompose(df):
         fig = decomposition.plot()
         plt.title(str(var))
         plt.show()
+
+
+def prep_prophet_data1(df: pd.DataFrame) -> pd.DataFrame:
+    return (df.assign(ds=pd.to_datetime(df.Date)).sort_values('ds')
+            .assign(y=df.Steps)
+            .groupby(['ds'])['Steps'].sum().reset_index().set_index('ds'))
+
+def prep_steps_data2(df: pd.DataFrame) -> pd.DataFrame:
+    return (df.assign(ds=pd.to_datetime(df.Date)).sort_values('ds')
+            .assign(y=df.Calories_Burned)
+            .groupby(['ds'])['Calories_Burned'].sum().reset_index().set_index('ds'))
+
+
+def prep_steps_data3(df: pd.DataFrame) -> pd.DataFrame:
+    return (df.assign(ds=pd.to_datetime(df.Date)).sort_values('ds')
+            .assign(y=df.Activity_Calories)
+            .groupby(['ds'])['Activity_Calories'].sum().reset_index().set_index('ds'))
+
 
